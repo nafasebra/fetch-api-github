f3f0e147d0da698c8212da8126875d90d58a9c3f
unknown <ebranafas@gmail.com>
2021-12-27 22:06:33 +0330

    add profile info component

--- src/component/profile/Avatar.js
+++ src/component/profile/Avatar.js
@@ -5,11 +5,14 @@ import "./Avatar.css";
 
 const Avatar = () => {
     return (
-        <div className='avatar__section'>
-            <div className='avatar__image'>
-                <img src="https://f.vividscreen.info/soft/b17ba1c70bfc81338843522be541202c/Invisible-1440x1280.jpg" alt="avatar" />
+        <div className="avatar">
+            <div className='avatar__section'>
+                <div className='avatar__image'>
+                    <img src="https://f.vividscreen.info/soft/b17ba1c70bfc81338843522be541202c/Invisible-1440x1280.jpg" alt="avatar" />
+                </div>
+                <div className='avatar__name'>Nafas Ebrahimi</div>
             </div>
-            <div className='avatar__name'>Nafas Ebrahimi</div>
+            <div className=''></div>
         </div>
     );
 }
--- src/component/profile/Profile.js
+++ src/component/profile/Profile.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 // import component
 import Avatar from './Avatar';
+import Profileinfo from './ProfileInfo';
 
 // import css file
 import "./Profile.css";
-
 
 const Profile = () => {
     return (
         <div className='profile__section'>
             <Avatar />
+            <Profileinfo />
         </div>
     );
 }
