9f34edd848dec4cf21c44da67003e740d60ca06e
unknown <ebranafas@gmail.com>
2022-01-01 21:05:26 +0330

    add repository components

---
+++ src/component/repository/Repositories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import component
+import repositoryItem from './RepositoryItem';
+
+//import css file
+import "./Repositories.css"
+
+
+const Repositories = () => {
+    return (
+        <div className="repositories__section">
+            
+        </div>
+    );
+}
+
+export default Repositories;
---
+++ src/component/repository/RepositoryItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import css file
+import "./RepositoryItem.css";
+
+const Repositoryitem = () => {
+
+    return (
+        <div className="repositoryitem">
+            
+        </div>
+    );
+}
+
+export default Repositoryitem;
