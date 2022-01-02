0ed67291e2256941c342f10aec365c96b0d574bc
unknown <ebranafas@gmail.com>
2022-01-02 23:23:22 +0330

    change style of repository component and add load more button

--- src/component/repository/Repositories.js
+++ src/component/repository/Repositories.js
@@ -10,8 +10,22 @@ import "./Repositories.css"
 const Repositories = () => {
     return (
         <div className="repositories__section">
-            <h2>Repositories</h2>
-            <RepositoryItem />
+            <div className="repositories__title">
+                <span classname="slash"></span>
+                <h2>Last Repositories</h2>
+            </div>
+            
+            <div className="repositories">
+                
+                <RepositoryItem />
+            </div>
+
+            <div className="load-more">
+                <p className="load-more__text">Load more</p>
+                <div className="load-more__icon">
+                    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
+                </div>
+            </div>
         </div>
     );
 }
