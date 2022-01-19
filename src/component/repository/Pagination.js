60209801d2c4c92674ed4af9d9f2f2bb99d76aa8
unknown <ebranafas@gmail.com>
2022-01-19 16:23:46 +0330

    add pagination compoennt

---
+++ src/component/repository/Pagination.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import css filr
+import './Pagination.css';
+
+const Pagination = () => {
+    return (
+        <div className="pagination__section">
+            
+        </div>
+    );
+}
+
+export default Pagination;
